e4e3902d25db988b3a1b9f6de59a47bb460d70e8
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-09-30 01:26:08 +0300

    theme to localstorage

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -35,6 +35,8 @@ function setActiveStyle(color) {
   alternateStyles.forEach((style) => {
     if (color === style.getAttribute("title")) {
       style.removeAttribute("disabled");
+      document.documentElement.style.setProperty("--skin-color", color);
+      localStorage.setItem("skin-color", color);
     } else {
       style.setAttribute("disabled", "true");
     }
@@ -48,6 +50,10 @@ dayNight.addEventListener("click", () => {
   dayNight.querySelector("i").classList.toggle("fa-sun");
   dayNight.querySelector("i").classList.toggle("fa-moon");
   document.body.classList.toggle("dark");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
 });
 window.addEventListener("load", () => {
   if (document.body.classList.contains("dark")) {
@@ -56,76 +62,3 @@ window.addEventListener("load", () => {
     dayNight.querySelector("i").classList.add("fa-moon");
   }
 });
-
-// ======================================================================================
-
-// const colors = ["#fb839e", "#ec9412", "#1fc586", "#2eb1ed", "#cc3a3b"];
-
-// const className = "color";
-
-// // сгенерить элементы для каждого из цветов
-// const colorSelect = document.querySelector(".colors");
-// colorSelect.innerHTML = colors
-//   .map(
-//     (color, i) =>
-//       `<span data-index="${i}" class="${className}" style="background: ${color}"></span>`
-//   )
-//   .join("\n");
-
-// // для новых посетителей
-// const defaultSettings = {
-//   index: 3,
-//   isDark: false,
-// };
-
-// // применить все настройки
-// const applySettings = (settings) => {
-//   const { index, isDark } = settings;
-
-//   // день-ночь
-//   document
-//     .querySelector(":root")
-//     .style.setProperty("--skin-color", colors[index]);
-
-//   // выбранный цвет
-//   const i = document.querySelector(".day-night i");
-//   i.classList[isDark ? "remove" : "add"]("fa-sun");
-//   i.classList[isDark ? "add" : "remove"]("fa-moon");
-// };
-
-// const LS_KEY = "myColorSettings";
-
-// // обработчик доступа к настройкам
-// const storeAndApply = {
-//   get(obj, prop) {
-//     return obj[prop];
-//   },
-//   set(obj, prop, value) {
-//     obj[prop] = value;
-//     localStorage.setItem(LS_KEY, JSON.stringify(obj));
-//     applySettings(obj);
-//     return true;
-//   },
-// };
-
-// const savedSettings = JSON.parse(localStorage.getItem(LS_KEY));
-// const settings = new Proxy(savedSettings || defaultSettings, storeAndApply);
-
-// // при загрузке
-// window.addEventListener("DOMContentLoaded", () => applySettings(settings));
-
-// // выбор цвета
-// colorSelect.addEventListener("click", (event) => {
-//   const { target } = event;
-//   if (!target.classList.contains(className)) {
-//     return;
-//   }
-
-//   const { index } = target.dataset;
-//   settings.index = Number(index);
-// });
-
-// // light or dark
-// document.querySelector(".day-night").addEventListener("click", () => {
-//   settings.isDark = !settings.isDark;
-// });
