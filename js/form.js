c0ba33ccea6bc1ff1a8c2b152b6ca751e2bafd86
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-10-01 23:00:28 +0300

    delete console.log

--- js/form.js
+++ js/form.js
@@ -1,60 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const form = document.querySelector(".contact-form");
-//   form.addEventListener("submit", formSend);
-
-//   async function formSend(event) {
-//     event.preventDefault();
-//     let error = formValidate(form);
-//     let formData = new FormData(form);
-//     if (error === 0) {
-//       form.classList.add("_sending");
-//       let response = await fetch("sendmail.php", {
-//         method: "POST",
-//         body: formData,
-//       });
-//       if (response.ok) {
-//         let result = await response.json();
-//         alert(result.message);
-//         form.reset();
-//         form.classList.remove("_sending");
-//       } else {
-//         alert("Error 🙄");
-//         form.classList.remove("_sending");
-//       }
-//     } else if (error !== 0) {
-//       alert("Please enter all areas 🤗");
-//       form.classList.remove("_sending");
-//     }
-//   }
-
-//   function formValidate(form) {
-//     let error = 0;
-//     let formReq = document.querySelectorAll("._req");
-//     for (let index = 0; index < formReq.length; index++) {
-//       const input = formReq[index];
-//       formRemoveError(input);
-
-//       if (input.classList.contains("_req")) {
-//         if (input.value === "") {
-//           formAddError(input);
-//           error++;
-//         }
-//       }
-//     }
-//     return error;
-//   }
-
-//   function formAddError(input) {
-//     input.parentElement.classList.add("_error");
-//     input.classList.add("_error");
-//   }
-
-//   function formRemoveError(input) {
-//     input.parentElement.classList.remove("_error");
-//     input.classList.remove("_error");
-//   }
-// });
-
 const TOKEN = "5439825587:AAFiZeos74ZIV8HS4lMgzvMsHS5iR1RJGPc";
 const CHAT_ID = "-1001518217052";
 const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
@@ -97,7 +40,6 @@ form.addEventListener("submit", function (e) {
         }, "3000");
       })
       .catch((error) => {
-        console.log(error);
         togglerErrorMessageClass();
         setTimeout(() => {
           togglerErrorMessageClass();
